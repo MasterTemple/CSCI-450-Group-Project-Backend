74490cde8274c850907a0f9d0c405505a8ec868e
Blake Scampone <54373793+MasterTemple@users.noreply.github.com>
2024-03-14 22:50:50 -0700

    Putting in 1 file

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
-from flask import Flask, request, jsonify
-from datetime import datetime as dt, timedelta
+####################
+# IMPORT LIBRARIES #
+####################
 
 from apscheduler.schedulers.background import BackgroundScheduler
-from uuid import uuid4
-from smtplib import SMTP
+from datetime import datetime as dt, timedelta
+from dotenv import load_dotenv
 from email.message import EmailMessage
-from pymongo import MongoClient
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from flask_cors import CORS
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
 from random import randint
+from smtplib import SMTP
+from uuid import uuid4
+
 import json
+import os
+import smtplib
+
+##############################
+# LOAD ENVIRONMENT VARIABLES #
+##############################
+
+load_dotenv()
+
+LOL_EMAIL = os.environ.get("EMAIL")
+LOL_PASSWORD = os.environ.get("APP_PASSWORD")
+
+if any(v is None for v in [LOL_EMAIL, LOL_PASSWORD]):
+    print("Follow the instructions in `README.md` to set up your `.env` file.")
+    exit(1)
+
+LOGIN_CODE_EXPIRATION_MINUTES = int(os.environ.get("LOGIN_CODE_EXPIRATION_MINUTES") or 5)
+AUTH_TOKEN_EXPIRATION_WEEKS = int(os.environ.get("AUTH_TOKEN_EXPIRATION_WEEKS") or 8)
 
 ########################################
 # REMEMBER TO START THE MONGOD SERVICE #
@@ -47,7 +72,7 @@ def is_valid_login_code(data):
             return True
     return False
 
-def is_athentic_user(data:dict):
+def is_athentic_user(data:dict) -> bool:
     try:
         email_address = data["email_address"]
         auth_token = data["auth_token"]
@@ -55,14 +80,31 @@ def is_athentic_user(data:dict):
         for tokens in tokens:
             if tokens["auth_token"] == auth_token:
                 return True
+        return False
     except:
         return False
 
-def get_user_from_auth_token(auth_token) -> str:
-    tokens = [c for c in email_codes.find({"email_address": email_address})]
-    for tokens in tokens:
-        if tokens["auth_token"] == auth_token:
-            return True
+def get_user_from_auth_token(auth_token) -> str | None:
+    entry = email_codes.find_one({"auth_token": auth_token})
+    if entry is None:
+        return None
+    return entry["email_address"]
+
+
+def send_verification_email(recipient: str, code: int):
+    SUBJECT = "Lyric of Lyrics - Verification Code"
+    BODY = f"Enter the following 6-digit code to verify your identity and gain access to your Lyric of Lyrics account: <b>{code}</b>"
+    s=smtplib.SMTP("smtp.gmail.com", 587)
+    s.starttls()
+    s.login(LOL_EMAIL, LOL_PASSWORD)
+    msg = MIMEMultipart()
+    msg['From'] = LOL_EMAIL
+    msg['To'] = recipient
+    msg['Subject'] = SUBJECT
+    html_message = BODY
+    msg.attach(MIMEText(html_message, 'html'))
+    s.sendmail(LOL_EMAIL, [recipient], msg.as_string())
+    s.quit()
 
 @app.route('/save', methods=['POST'])
 def save():
@@ -113,17 +155,6 @@ def load():
     # return list of songs
     return jsonify(result)
 
-HOST_EMAIL = ""
-
-def send_email(address: str, code: int):
-    email = EmailMessage()
-    email.set_content(f"Here is your code: {code}")
-    email['Subject'] = "Lyric of Lyrics - Verification Code"
-    email['From'] = HOST_EMAIL
-    email['To'] = address
-    with SMTP("localhost") as s:
-        s.send_message(email)
-
 @app.route('/send_verification_code', methods=['POST'])
 def send_verification_code():
     # get json data sent in body of request
@@ -140,7 +171,7 @@ def send_verification_code():
 
     # email user
     print(f"Sent code {login_code} to '{email_address}'")
-    # send_email(email_address, login_code)
+    send_verification_email(email_address, login_code)
     return jsonify({})
 
 
@@ -160,13 +191,9 @@ def verify_login():
         user_logins.insert_one(json.loads(json.dumps(entry)))
     return jsonify(entry)
 
-LOGIN_CODE_EXPIRATION_MINUTES = 5
-
 def remove_old_login_codes():
     window = dt.now() - timedelta(minutes=LOGIN_CODE_EXPIRATION_MINUTES)
     email_codes.delete_many({"time": {"$lt": window.isoformat()}})
-
-AUTH_TOKEN_EXPIRATION_WEEKS = 8
 
 def remove_old_auth_tokens():
     window = dt.now() - timedelta(weeks=AUTH_TOKEN_EXPIRATION_WEEKS)
